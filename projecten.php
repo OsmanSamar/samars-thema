9cdf8d389891717ff967e4e60dc4633cf3ba05e9
Samar <osmansamar367@gmail.com>
2024-10-01 09:36:10 +0200

    Added Swiper on Small Screen and maked the img Grind on Lg Screen

--- projecten.php
+++ projecten.php
@@ -97,6 +97,11 @@
                 <span class="last">Projecten</span>
             </div>
 
+
+
+
+
+
             <!-- First Container -->
             <div class="container d-lg-grid d-none  " style="margin-top: 300px">
                 <div class="row ">
@@ -113,9 +118,15 @@
 
 
 
-                        <div class="grid-item" style="grid-row: span 2;  ">
+                        <div class="grid-item " style="grid-row: span 2;  margin-top:140px; ">
                             <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled11.jpg" alt="scaled11"
                                 class="object-fit-cover rounded" style="height:400px;" />
+
+                        </div>
+                        <div class="grid-item mt-4" style="grid-row: span 2;  ">
+                            <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled13.jpg" alt="scaled13"
+                                class="object-fit-cover rounded" style="height:900px;" />
+
                         </div>
                     </div>
 
@@ -124,13 +135,18 @@
                             <img src="<?= esc_url(get_template_directory_uri()); ?>/images/chinaoldgarden.png"
                                 alt="chinaoldgarden" class="img1 object-fit-cover rounded" />
                         </div>
-                        <div class="grid-7">
+                        <div class="grid-7 mt-4">
                             <div class="row me-0">
                                 <div class="col-12">
                                     <div class="grid-item  ratio ratio-custom" style="--x:53;--y:67;">
                                         <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled19.jpg"
                                             alt="scaled19" class="object-fit-cover rounded"
-                                            style="height:550px; width:; margin-left:; " />
+                                            style="height:; width:; margin-left:; " />
+                                    </div>
+                                    <div class="grid-item  ratio ratio-custom mt-4" style="--x:53;--y:67;">
+                                        <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled51.jpg"
+                                            alt="scaled19" class="object-fit-cover rounded"
+                                            style="height:; width:; margin-left:; " />
                                     </div>
                                 </div>
                             </div>
@@ -140,7 +156,12 @@
                                     <div class="grid-item ratio ratio-custom" style="--x:39;--y:53;">
                                         <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled5.jpg"
                                             alt="scaled5" class="object-fit-cover rounded"
-                                            style="height:450px; width:330px; margin-left:;" />
+                                            style="height:; width:; margin-left:;" />
+                                    </div>
+                                    <div class="grid-item ratio ratio-custom mt-4" style="--x:39;--y:53;">
+                                        <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled65.jpg"
+                                            alt="scaled65" class="object-fit-cover rounded"
+                                            style="height:; width:; margin-left:;" />
                                     </div>
                                 </div>
                             </div>
@@ -148,77 +169,11 @@
                     </div>
 
 
-                    <div class="image-grid col-12 d-lg-grid ">
 
-
-
-
-                        <div class="grid-item" style="grid-row: span 2;">
-                            <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled51.jpg" alt="scaled13"
-                                class="object-fit-cover rounded"
-                                style="height:450px; width:330px ; margin-left:115px;" />
-                        </div>
-                        <div class="grid-item" style="">
-                            <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled13.jpg" alt="scaled65"
-                                class="object-fit-cover rounded" style="height:800px;  " />
-                        </div>
-                        <div class="grid-item" style="; grid-row: span 2;">
-                            <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled65.jpg" alt="scaled51"
-                                class="object-fit-cover rounded" style="height:400px; width:; margin-left:;" />
-                        </div>
-
-                    </div>
                 </div>
                 <div class="lin3"></div>
 
-
-
             </div>
-
-
-            <!-- 2 -->
-            <!-- <div class="row">
-                <div class="col-12 col-lg-5 mb-4">
-                    <div class="text-header">Projecten </div>
-                    <div class="text">
-                        <div class="circle-border">
-                            <a href="#" class="btn btn-primary"
-                                style="padding-left: 40px;padding-right: 40px; font-size:18px;">kennismaken?</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-7 mb-4 text-center d-none d-md-none d-lg-flex">
-                    <img src=" <?= esc_url(get_template_directory_uri()); ?>/images/chinaoldgarden.png"
-                        alt="chinaoldgarden" class="img1 object-fit-cover rounded" />
-                </div>
-
-                <div class="col-12 col-lg-5 mb-4 text-center d-none  d-none d-md-none d-lg-flex ">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled11.jpg" alt="scaled11"
-                        class="object-fit-cover rounded  " style="height:400px; margin-bottom:60px;" />
-                </div>
-                <div class="col-12 col-lg-4 mb-4 text-center d-none  d-none d-md-none d-lg-flex">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled19.jpg" alt=" scaled19"
-                        class="object-fit-cover rounded" style="height:600px;" />
-                </div>
-                <div class=" col-12 col-lg-3 mb-4 text-center d-none d-none d-md-none d-lg-flex">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled5.jpg" alt="scaled5"
-                        class="object-fit-cover rounded" style="height:400px;" />
-                </div>
-
-                <div class="col-12 col-lg-5 mb-4 text-center d-none  d-none d-md-none d-lg-flex ">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled13.jpg" alt="scaled13"
-                        class="object-fit-cover rounded" style="height:800px;" />
-                </div>
-                <div class="col-12 col-lg-4 mb-4 text-center d-none  d-none d-md-none d-lg-flex">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled65.jpg" alt=" scaled65"
-                        class="object-fit-cover rounded" style="height:300px;" />
-                </div>
-                <div class=" col-12 col-lg-3 mb-4 text-center d-none d-none d-md-none d-lg-flex">
-                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled51.jpg" alt="scaled51"
-                        class="object-fit-cover rounded" style="height:400px;" />
-                </div>
-
-            </div> -->
 
 
         </div>
@@ -227,7 +182,7 @@
 
 
     </div>
-    <!-- 1 -->
+    <!-- The first swiper  -->
     <div class="container">
 
         <!-- Swiper On Small Screen -->
@@ -254,7 +209,7 @@
         </div>
 
     </div>
-    <!-- 2 -->
+    <!-- The second swiper -->
 
     <div class="container">
 
@@ -286,6 +241,42 @@
         </div>
 
     </div>
+
+
+    <!-- The third swiper -->
+
+    <div class="container">
+
+        <!-- Swiper On Small Screen -->
+        <div #swiperRef="" class="swiper mySwiper smrow order-2 projecten-slider  d-flex d-md-flex d-lg-none"
+            style="--swiper-navigation-color: #f5a287; --swiper-pagination-color: #f5a287">
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide">
+                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled5.jpg" alt="scaled5"
+                        class="object-fit-cover rounded" style="" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled51.jpg" alt="scaled13"
+                        class="object-fit-cover rounded" style="" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled13.jpg" alt="scaled65"
+                        class="object-fit-cover rounded" style="  " />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?= esc_url(get_template_directory_uri()); ?>/images/scaled65.jpg" alt="scaled51"
+                        class="object-fit-cover rounded" style="" />
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+    </div>
+
+
     <div class="bg-leaf">
 
         <div>
@@ -316,8 +307,7 @@
 
 
 
-    </div>
-    </div>
+
 
     <!-- Swiper JS -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js">
