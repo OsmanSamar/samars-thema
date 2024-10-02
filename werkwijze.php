177216bf8cf1014ce12ac677ec658327c7666eb0
Samar <osmansamar367@gmail.com>
2024-10-02 15:31:56 +0200

    Update Contact Section on Lg Screen

--- werkwijze.php
+++ werkwijze.php
@@ -537,26 +537,32 @@
 
         <!-- Contact Information Section on Lg Screen -->
 
-        <div>
-            <div class="  flex-lg-row  d-flex flex-column align-items-center d-none d-lg-flex"
+        <div class="container">
+            <div class="  flex-lg-row  d-flex  align-items-center justify-content-center d-none d-lg-flex"
                 style="background-color:#2A3F31; border-radius: 50px; margin-top:40px; margin-bottom:40px;gap-20px; padding:20px ">
                 <div class="d-flex justify-content-center justify-content-lg-start">
                     <img src="<?= esc_url(get_template_directory_uri()); ?>/images/avatar.png" alt="Avatar Image"
                         class="groentuinen" style="width:80px; height:80px" />
                 </div>
-                <div class=" text-center text-lg-left" style="margin-left:50px; font-size: 30px;">
+                <div class=" text-center text-lg-left" style="margin-left:50px; 
+                font-size: 30px; 
+                font-weight:300; 
+                line-height:2px;
+                  font-family: 'Henderson Sans-basic', sans-serif;">
+
+
                     <span>Samen met ons aan de slag? Neem contact op!</span>
                 </div>
                 <div class="button-group" style="margin-left:50px">
                     <div class="circle-border ">
                         <a class="btn btn-secondary" aria-label="telefoon Contact" href="tel:+31621830806"
-                            style="padding-left: 40px;padding-right: 30px"> +31
+                            style="padding-left: 20px;padding-right: 20px;font-size:18px;"> +31
                             6
                             21830806</a>
                     </div>
                     <div class="circle-border">
                         <a class="btn btn-primary" aria-label="Mail Contact" href="mailto:info@groentuinen.nl"
-                            style="padding-left: 40px;padding-right: 30px">info@groentuinen.nl</a>
+                            style="padding-left: 30px;padding-right: 20px;font-size:18px;">info@groentuinen.nl</a>
                     </div>
                 </div>
             </div>
